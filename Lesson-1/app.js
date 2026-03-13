a0eb0a00748e23df4c117404b167aa1f4a9cf95a
SakshyamB <sakshyamb23@gmail.com>
2026-03-13 22:08:03 +0545

    Initial commit for intro to express and middleware in express.js

---
+++ Lesson-1/app.js
@@ -0,0 +1,21 @@
+// core module
+const http = require('http');
+// external module
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+    console.log("Middleware first", req.url, req.method);
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("Middleware second", req.url, req.method);
+    res.send("<p> you are welcome to express.js </p>");
+})
+
+
+const server = http.createServer(app);
+
+const PORT = 3001;
+server.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
