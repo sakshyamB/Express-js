82be66ba5379d8100253fc55313a2b24f00dede9
SakshyamB <sakshyamb23@gmail.com>
2026-05-24 20:02:01 +0545

    a

--- Lesson-9/models/hotelModel.js
+++ Lesson-9/models/hotelModel.js
@@ -1,25 +1,21 @@
 const { getDB } = require("../db/db");
 const { ObjectId } = require("mongodb");
 
-// GET ALL
 exports.getAllHotels = async () => {
     const db = getDB();
     return await db.collection("hotels").find().toArray();
 };
 
-// ADD (you already have this, just in case)
 exports.addHotel = async (hotel) => {
     const db = getDB();
     return await db.collection("hotels").insertOne(hotel);
 };
 
-// FIND BY ID
 exports.getHotelById = async (id) => {
     const db = getDB();
     return await db.collection("hotels").findOne({ _id: new ObjectId(id) });
 };
 
-// UPDATE
 exports.updateHotel = async (id, updatedData) => {
     const db = getDB();
 
@@ -31,7 +27,6 @@ exports.updateHotel = async (id, updatedData) => {
     );
 };
 
-// DELETE
 exports.deleteHotel = async (id) => {
     const db = getDB();
 
